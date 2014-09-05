4550618ba8ab5fae5a4e5cf716a774a100a3c6bd
Robert Kaye <rob@musicbrainz.org>
2014-09-05 11:44:48 +0000

    First attempt at websockets using flask-websocket. Not working. :(

--- graph.py
+++ graph.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+import sys
+import socket
+import fcntl
+import struct
+from Queue import Queue
+from threading import Thread, Event
+from flask import Flask, render_template, request
 from flask_sockets import Sockets
 from pendulum import Pendulum
 
@@ -8,11 +14,13 @@ STATIC_PATH = "/static"
 STATIC_FOLDER = "static"
 TEMPLATE_FOLDER = "template"
 
+p = None
+
 app = Flask(__name__,
             static_url_path = STATIC_PATH,
             static_folder = STATIC_FOLDER,
             template_folder = TEMPLATE_FOLDER)
-#sockets = Sockets(app)
+sockets = Sockets(app)
 
 class GraphPendulum(Pendulum):
 
@@ -20,35 +28,47 @@ class GraphPendulum(Pendulum):
         Pendulum.__init__(self, update_interval)
         self.data = []
         self.num_points = num_points
+        self.ws_queue = Queue()
+        self.ws_event = Event()
 
     def process_data(self, data):
-#        print "%.03f: %.4f, %.4f, %.4f" % (data['t'], data['x'], data['y'], data['z'])
-        self.data.append(data)
-        if len(self.data) > self.num_points:
-            self.exit()
+        self.data = "%.4f,%.4f,%.4f,%.4f" % (data['t'], data['x'], data['y'], data['z'])
+        self.ws_event.set()
 
-    def graph_data(self, k):
-        fmt = []
-        for d in self.data:
-            fmt.append('%.3f' % d[k])
+class Background(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.pendulum = None
 
-        data =  ",".join(fmt)
-        return data
+    def run(self):
+        self.pendulum = GraphPendulum(.1, 20)
+        self.pendulum.load_calibration()
+        self.pendulum.process()
 
+def get_ip_address_from_interface(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,  
+                                struct.pack('256s', ifname[:15]))[20:24])
+    except IOError:
+        return "[none]"
+
+host = get_ip_address_from_interface("eth0")
+port = 8000
 
 @app.route('/') 
 def hello(): 
-    return render_template("index")
+    return render_template("index", host=host, port=port)
 
-#p = GraphPendulum(.5, 20)
-#p.load_calibration()
-#try:
-#    p.process()
-#except KeyboardInterrupt:
-#    p.exit()
-#    print
+p = Background()
+@sockets.route('/pendulum') 
+def pendulum_socket(ws): 
+    print "Socket connected!"
+    while True: 
+        p.pendulum.ws_event.wait()    
+        ws.send(p.data)
 
-#print html % (p.graph_data('t'), p.graph_data('x'), p.graph_data('y'), p.graph_data('z'))
+p.start()
 
-app.run(debug=True, host="0.0.0.0", port=8080)
-
+if __name__ == '__main__':
+    app.run(debug=True, host=host, port=port)
